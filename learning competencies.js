a72974d2130a36e41bf9e5e16b7a0866e50ae62d
niskarsh <niskarshk@gmail.com>
2020-03-19 12:27:03 +0530

    First commit

---
+++ learning competencies.js
@@ -0,0 +1,7 @@
+// DOM - doc as tree str of objects
+// https://en.wikipedia.org/wiki/Document_Object_Model
+// display : inline, inline-block, block, none, flex
+// siblings, selectors https://www.w3schools.com/css/css_combinators.asp, pseudo-classes https://developer.mozilla.org/en-US/docs/Web/CSS/Pseudo-classes
+// postion: static, absolute, relative
+// box sizing
+// https://www.w3schools.com/css/css_rwd_viewport.asp
